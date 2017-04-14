967d6bff96487719b0cf8d987804d01d28279177
Michał Teodorowicz-Wilga <teodorowicz@gmail.com>
2017-04-14 18:59:58 +0200

    wersja pierwsza apki optiviewer

---
+++ www/js/sc_ajax_jmobile.js
@@ -0,0 +1,88 @@
+var daneSrodka;
+
+        $(function() {
+            //console.log('start...');
+            $("#callAjax").click(function() {
+                var kodEan = $.trim($("#Kod").val());
+                //console.log(kodEan);
+                
+                if(kodEan.length == 10)
+                {
+                    $.ajax({
+                      type: "GET",
+                      url: "http://intranet2.su.krakow.pl/~mwilga/opti.php",
+                      data: ({srd_ean: kodEan}),
+                      cache: false,
+                      dataType: "json",
+                      success: onSuccess
+                    })
+                }
+            });
+ 
+            $("#result").ajaxError(function(event, request, settings, exception) {
+              $("#result").html("Error Calling: " + settings.url + "<br />HTTP Code: " + request.status);
+            });
+ 
+            function onSuccess(data)
+            {   
+                if (typeof(data)==='undefined'||data===null||!data.hasOwnProperty('srd_ean')) return false;
+                
+                daneSrodka=data;
+                
+                //let elem = document.createElement('div');
+                $('<div>')
+                    .attr('id','ean'+data.srd_ean)
+                    .attr('data-role','controlgroup')
+                    .attr('data-type','horizontal')
+                    .append(
+                        $("<a>")
+                            .attr('href','#page'+data.srd_ean)
+                            .html(data.srd_ean+' - '+data.srd_nazwa)
+                            .addClass('ui-shadow ui-btn ui-corner-all ui-btn-inline ui-btn-icon-left ui-icon-eye') 
+                        )
+                    .append(
+                        $("<a>")
+                            .attr('href','#')
+                            .html("The die is cast.")
+                            .addClass('"ui-shadow ui-btn ui-corner-all ui-btn-inline ui-icon-delete ui-btn-icon-notext ui-btn-b')
+                            .click( function () {
+                                    $(this).parent().remove()
+                                    $('#page'+data.srd_ean).remove()
+                                }
+                            )
+                        )
+                    .appendTo($("#result"));
+
+                $('<div>')
+                    .attr('id','page'+data.srd_ean)
+                    .attr('data-role','page')
+                    .append(
+                        $('<div>')
+                        .attr('data-role','header')
+                        .html(data.srd_ean+ ' - '+data.srd_nazwa)
+                    )
+                    .append(
+                        $('<div>')
+                        .attr('data-role','content')
+                        .append(
+                            $('<div>')
+                            .attr('data-role','collapsible')
+                            .attr('data-collapsed-icon',"carat-d")
+                            .attr('data-expanded-icon',"carat-u")
+                            .html('<h4>Dane podstawowe</h4>')
+                        )
+                    )
+                    //.html('Kod kreskowy:'+ data.srd_ean)
+                    .appendTo('body');
+                    //href="javascript:history.back()"
+            }
+ 
+        });
+
+
+
+
+
+
+
+
---
+++ www/js/test.js
@@ -0,0 +1,10 @@
+
+
+
+function ajwaj(){
+    alert('nie nie dziala!!');
+};
+
+$( "body" ).click(function( event ) {
+  console.log( "clicked: " + event.target );
+});
