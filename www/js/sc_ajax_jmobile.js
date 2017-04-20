e977dd2fca0434d3a312ddc47a4ec3f3bb3fa44a
Michał Teodorowicz-Wilga <teodorowicz@gmail.com>
2017-04-20 13:03:29 +0200

    testowanie pluginu do barcoddescanera - nie dziala plugin

--- www/js/sc_ajax_jmobile.js
+++ www/js/sc_ajax_jmobile.js
@@ -1,5 +1,7 @@
 var daneSrodka;
 
+
+/*
 $('#skanuj').click(
     function(){
         cordova.plugins.barcodeScanner.scan(
@@ -13,7 +15,7 @@ $('#skanuj').click(
                 alert("Scanning failed: " + error);
             },
             {
-                preferFrontCamera : true, // iOS and Android 
+            preferFrontCamera : true, // iOS and Android 
             showFlipCameraButton : true, // iOS and Android 
             showTorchButton : true, // iOS and Android 
             torchOn: true, // Android, launch with the torch switched on (if available) 
@@ -27,10 +29,10 @@ $('#skanuj').click(
         );
     }
 );
+*/
 
 function getApiUrl () {
-
-if (localStorage.getItem("optiest-mwapi")===null){
+    if (localStorage.getItem("optiest-mwapi")===null){
 
 /*
 $('div')       
@@ -76,23 +78,6 @@ $('div')
 else 
 $('#inputURL').attr('value',localStorage.getItem("optiest-mwapi"))
 return localStorage.getItem("optiest-mwapi");
-/*
-<a href="#popupLogin" data-rel="popup" data-position-to="window" 
-class="ui-btn ui-corner-all ui-shadow ui-btn-inline ui-icon-check ui-btn-icon-left ui-btn-a" 
-data-transition="pop">Sign in</a>
-<div data-role="popup" id="popupLogin" data-theme="a" class="ui-corner-all">
-    <form>
-        <div style="padding:10px 20px;">
-            <h3>Please sign in</h3>
-            <label for="un" class="ui-hidden-accessible">Username:</label>
-            <input type="text" name="user" id="un" value="" placeholder="username" data-theme="a">
-            <label for="pw" class="ui-hidden-accessible">Password:</label>
-            <input type="password" name="pass" id="pw" value="" placeholder="password" data-theme="a">
-            <button type="submit" class="ui-btn ui-corner-all ui-shadow ui-btn-b ui-btn-icon-left ui-icon-check">Sign in</button>
-        </div>
-    </form>
-</div>
-*/
 }
 
 function setApiUrl() {
@@ -108,6 +93,24 @@ function setApiUrl() {
             //$('#formGetURL').submit(function(){return false} );
 
             urlApi=getApiUrl();
+
+
+if (typeof cordova!=='undefined') {
+$('#skanuj').click(
+    cordova.plugins.barcodeScanner.scan(
+      function (result) {
+          alert("We got a barcode\n" +
+                "Result: " + result.text + "\n" +
+                "Format: " + result.format + "\n" +
+                "Cancelled: " + result.cancelled);
+      }, 
+      function (error) {
+          alert("Scanning failed: " + error);
+      }
+    )
+   );
+
+}
 
             $("#callAjax").click(function() {
                 var kodEan = $.trim($("#Kod").val());
